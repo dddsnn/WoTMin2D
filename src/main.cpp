d265861cae5d1fb3db33ca0ab3103406fa1b8e46
dddsnn <marc.lehmann@gmx.de>
2017-02-02 11:44:53 +0100

    Add SDL2
    Just the library include, and initialization and shutdown.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <SDL.h>
 
 int main(int, char**) {
-
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << SDL_GetError() << std::endl;
+        return -1;
+    }
+    SDL_Quit();
+    return 0;
 }
