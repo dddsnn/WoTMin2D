699c74619e6a5e0d21d5d6acef23cede94936c1d
dddsnn <marc.lehmann@gmx.de>
2017-02-05 01:32:46 +0100

    Implement Screen setup
    The Screen class now properly manages its resources and is capable of
    drawing pixels into its window.
    
    For testing, it draws a 100 pixel line at coordinates (100, 100).

--- src/display/Screen.cpp
+++ src/display/Screen.cpp
@@ -2,8 +2,90 @@
 
 namespace wotmin2d {
 
-void Screen::draw(const State& state) {
+void Screen::construct(unsigned int width, unsigned int height) {
+    window = SDL_CreateWindow("WoTMin2D", SDL_WINDOWPOS_CENTERED,
+                              SDL_WINDOWPOS_CENTERED, width, height, 0);
+    if (window == nullptr) {
+        throw SdlException("Error creating a window.", SDL_GetError());
+    }
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if (renderer == nullptr) {
+        SDL_DestroyWindow(window);
+        throw SdlException("Error creating a renderer.", SDL_GetError());
+    }
+    try {
+        texture.reset(new SdlTexture(renderer, width, height));
+    } catch (...) {
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        throw;
+    }
+}
 
+Screen::Screen(unsigned int width, unsigned int height) :
+    window(nullptr),
+    renderer(nullptr),
+    texture(nullptr)
+{
+    construct(width, height);
+}
+
+Screen::Screen(const Screen& other) :
+    window(nullptr),
+    renderer(nullptr),
+    texture(nullptr)
+{
+    int width, height;
+    SDL_GetWindowSize(other.window, &width, &height);
+    construct(width, height);
+}
+
+Screen& Screen::operator=(Screen other) {
+    swap(*this, other);
+    return *this;
+}
+
+Screen::~Screen() {
+    if (renderer != nullptr) {
+        SDL_DestroyRenderer(renderer);
+    }
+    if (window != nullptr) {
+        SDL_DestroyWindow(window);
+    }
+}
+
+void swap(Screen& first, Screen& second) noexcept {
+    using std::swap;
+    swap(first.window, second.window);
+    swap(first.renderer, second.renderer);
+    swap(first.texture, second.texture);
+}
+
+void Screen::draw(const State& state) {
+    updateTexture(state);
+    presentTexture();
+}
+
+void Screen::updateTexture(const State& state) {
+    texture->lockForWriting();
+    // TODO
+    // Make a 100 pixel line 100 pixels into the texture for testing
+    for (int i = 100; i < 200; i++) {
+        texture->setPixel(100 * texture->getWidth() + i, 0xff, 0x00, 0x00);
+    }
+    texture->unlock();
+}
+
+void Screen::presentTexture() {
+    if (SDL_RenderClear(renderer) != 0) {
+        throw SdlException("Error clearing the renderer.", SDL_GetError());
+    }
+    if (SDL_RenderCopy(renderer, texture->getTexture(), nullptr,
+                       nullptr) != 0) {
+        throw SdlException("Error copying a texture to the renderer.",
+                           SDL_GetError());
+    }
+    SDL_RenderPresent(renderer);
 }
 
 }
--- src/display/Screen.hpp
+++ src/display/Screen.hpp
@@ -2,12 +2,31 @@
 #define SCREEN_HPP
 
 #include "../game/State.hpp"
+#include "SdlTexture.hpp"
+#include "SdlException.hpp"
+
+#include <SDL.h>
+#include <utility>
+#include <cstdint>
+#include <memory>
 
 namespace wotmin2d {
 
 class Screen {
     public:
+    Screen(unsigned int width, unsigned int height);
+    Screen(const Screen& other);
+    Screen& operator=(Screen other);
+    ~Screen();
+    friend void swap(Screen& first, Screen& second) noexcept;
     void draw(const State& state);
+    private:
+    void construct(unsigned int width, unsigned int height);
+    void updateTexture(const State& state);
+    void presentTexture();
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    std::unique_ptr<SdlTexture> texture;
 };
 
 }
