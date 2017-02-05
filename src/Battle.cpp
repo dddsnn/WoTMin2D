c75588f6780f45bd0373ca9daf272b62f8f85ea6
dddsnn <marc.lehmann@gmx.de>
2017-02-05 16:39:14 +0100

    Let Battle render
    Battle now renders 200 frames at 100 frames per second for testing.

--- src/Battle.cpp
+++ src/Battle.cpp
@@ -2,13 +2,25 @@
 
 namespace wotmin2d {
 
+Battle::Battle(unsigned int width, unsigned int height) :
+    screen(width, height),
+    state() {
+}
+
 void Battle::start() {
     // TODO implement stopping
-    while (true) {
+    std::uint_fast32_t time = SDL_GetTicks();
+    // Render 200 frames at 100 fps.
+    for (int i = 0; i < 200; i++) {
         state.update();
         screen.draw(state);
         // TODO unhardcode time step
-        SDL_Delay(10);
+        std::uint_fast32_t new_time = SDL_GetTicks();
+        if (new_time < time + 10) {
+            // Wait some time so that the new frame begins after the right time.
+            SDL_Delay(time + 10 - new_time);
+        }
+        time = new_time;
     }
 }
 
--- src/Battle.hpp
+++ src/Battle.hpp
@@ -5,11 +5,13 @@
 #include "display/Screen.hpp"
 
 #include <SDL_timer.h>
+#include <cstdint>
 
 namespace wotmin2d {
 
 class Battle {
     public:
+    Battle(unsigned int width, unsigned int height);
     void start();
     void stop();
 
